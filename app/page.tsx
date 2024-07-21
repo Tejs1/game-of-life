129b98bf6a9cc61b671155df049ce1d8023f8b2d
Tejs1 <tejsrelax@gmail.com>
2024-07-21 11:24:16 +0000

    ts error

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
 	return (
 		<main className="flex min-h-screen flex-col items-center justify-between p-24">
 			<div className="App">
-				<h1>Conway's Game of Life</h1>
+				<h1>Conway&apos;s Game of Life</h1>
 				<GameOfLife />
 			</div>
 		</main>
